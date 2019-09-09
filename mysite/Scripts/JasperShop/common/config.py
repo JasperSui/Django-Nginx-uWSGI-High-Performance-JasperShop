cf994e52f6736f811a4a3d961641a25b0d9aec50
Jasper Sui <suiyang03@gmail.com>
2019-09-10 05:10:48 +0800

    Edit base_url for cloud version

--- mysite/Scripts/JasperShop/common/config.py
+++ mysite/Scripts/JasperShop/common/config.py
@@ -1,1 +1,8 @@
-base_url = 'http://127.0.0.1:8000/'
+import os
+
+os_name = os.name
+
+if os_name = 'nt':
+    base_url = 'http://127.0.0.1:8000/'
+elif os_name = 'posix':
+    base_url = 'https://jaspersui.club/'
