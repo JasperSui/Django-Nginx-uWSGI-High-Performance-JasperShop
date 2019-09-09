1bff70ffdb784f712210086266d24a64bf5827e0
Jasper Sui <suiyang03@gmail.com>
2019-09-10 05:35:27 +0800

    Add linux root permission for celery

--- mysite/Scripts/JasperShop/JasperShop/celery.py
+++ mysite/Scripts/JasperShop/JasperShop/celery.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 import os
-from celery import Celery
+from celery import Celery, platforms
 from django.conf import settings
+
+platforms.C_FORCE_ROOT = True
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
