81eead5624121439f099ff7b5fa259f9503c0942
Jasper Sui <suiyang03@gmail.com>
2019-09-10 05:11:54 +0800

    Exchange = to ==

--- mysite/Scripts/JasperShop/common/config.py
+++ mysite/Scripts/JasperShop/common/config.py
@@ -2,7 +2,7 @@ import os
 
 os_name = os.name
 
-if os_name = 'nt':
+if os_name == 'nt':
     base_url = 'http://127.0.0.1:8000/'
-elif os_name = 'posix':
+elif os_name == 'posix':
     base_url = 'https://jaspersui.club/'
